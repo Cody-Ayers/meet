811e0e98837747baac0bf091917439abe083c253
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2024-01-29 15:48:15 -0700

    updated spi.js

--- src/api.js
+++ src/api.js
@@ -63,6 +63,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -74,6 +79,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
