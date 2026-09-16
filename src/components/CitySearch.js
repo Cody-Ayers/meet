ff7b7edb4a07df7d6e7a8b9e09fb61a5b65a61c4
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2026-09-15 23:54:45 -0600

    Fix CitySearch suggestions

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -2,14 +2,19 @@
 
 import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
+const CitySearch = ({ allLocations = [], setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
+  // Sync suggestions whenever allLocations array or length changes
   useEffect(() => {
-    setSuggestions(allLocations);
-  }, [allLocations]); // Updated dependency array
+    if (allLocations && allLocations.length) {
+      setSuggestions(allLocations);
+    } else {
+      setSuggestions([]);
+    }
+  }, [allLocations]);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
@@ -18,13 +23,14 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
           return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         })
       : [];
+
     setQuery(value);
     setSuggestions(filteredLocations);
 
     let infoText;
-    if (filteredLocations.length === 0) {
+    if (value.length > 0 && filteredLocations.length === 0) {
       infoText =
-        "We can not find the city you are looking for. Please try another city";
+        "We cannot find the city you are looking for. Please try another city.";
     } else {
       infoText = "";
     }
@@ -51,13 +57,17 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
       />
       {showSuggestions ? (
         <ul className="suggestions">
-          {suggestions.map((suggestion) => {
-            return (
-              <li onClick={handleItemClicked} key={suggestion}>
+          {suggestions && suggestions.length > 0 ? (
+            suggestions.map((suggestion) => (
+              <li key={suggestion} onClick={handleItemClicked}>
                 {suggestion}
               </li>
-            );
-          })}
+            ))
+          ) : (
+            <li key="no-results" style={{ fontStyle: "italic", color: "#888" }}>
+              No cities found
+            </li>
+          )}
           <li key="See all cities" onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
